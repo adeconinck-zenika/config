602c2e629d87934ceae394811318bd5e45b9d94b
neyb <noo.blaster@gmail.com>
2017-01-04 17:46:32 +0100

    fish: add bindirs creation when starting with "!"

--- .config/fish/inits/bindirs.fish
+++ .config/fish/inits/bindirs.fish
@@ -1,8 +1,16 @@
 for bindir in $argv[-1..1]
   set bindir (eval echo $bindir)
-  #if [ ! -d $bindir ]
-  #  mkdir -p $bindir
-  #end
+
+  # ! at the beginning of the line to create directory
+  if string match '!*' $bindir >/dev/null
+    set bindir (string sub -s2 $bindir) #remove '!'
+    set bindir (eval echo $bindir) #reevaluate bindir : ! fuck the normal eval
+    if [ ! -d $bindir ]
+      mkdir -p $bindir
+    end
+  end
+
+  # add in path if does not exist
   if not contains $bindir $PATH
     set PATH $bindir $PATH
   end
