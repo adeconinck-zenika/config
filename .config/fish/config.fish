67c55af7274a5457db90962b2ff64b317d0dcbef
neyb <noo.blaster@gmail.com>
2017-01-05 13:58:15 +0100

    fish: full path for inits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 begin
-  for f in (cat .config/fish/inits/sources)
-    source .config/fish/inits/$f.fish (cat .config/fish/inits/conf/$f 2>/dev/null)
+  for f in (cat ~/.config/fish/inits/sources)
+    source ~/.config/fish/inits/$f.fish (cat ~/.config/fish/inits/conf/$f 2>/dev/null)
   end
 end
