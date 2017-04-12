68e223309bc438040e84d0ebd1b46da7b2f868da
neyb <noo.blaster@gmail.com>
2017-04-12 23:11:48 +0200

    disable fisher in init

--- .config/fish/inits/fish.fish
+++ .config/fish/inits/fish.fish
@@ -1,2 +1,2 @@
-set -g fish_function_path ~/.config/fish/myFunctions $fish_function_path
-set -g fish_complete_path ~/.config/fish/myCompletions $fish_complete_path
+set -g fish_function_path ~/.config/fish/myFunctions ~/.config/fish/subtreesFunctions $fish_function_path
+set -g fish_complete_path ~/.config/fish/myCompletions ~/.config/fish/subtreesCompletions $fish_complete_path
