46553fb6d372313ef365fe7c05dba0582ee0d89d
neyb <noo.blaster@gmail.com>
2017-04-13 08:52:52 +0200

    jenv: move config folder in .config

--- .config/fish/inits/jenv.fish
+++ .config/fish/inits/jenv.fish
@@ -1,2 +1,3 @@
-mkdir -p ~/.jenv/shims ~/.jenv/versions ~/.jenv/plugins
-add2path ~/.jenv/shims
+set -x JENV_ROOT ~/.config/jenv
+mkdir -p $JENV_ROOT/shims $JENV_ROOT/versions $JENV_ROOT/plugins
+add2path $JENV_ROOT/shims
